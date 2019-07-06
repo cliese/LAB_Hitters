861766bc13544dad4712e2c562fea986e7392fc2
carsten <liese.math@gmail.com>
2019-07-06 11:37:10 +0200

    completed get models in step 1 of forward selection

--- Hitters_main.py
+++ Hitters_main.py
@@ -3,31 +3,15 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
-df_hitters = pd.read_csv('/Users/Caddi/Data/Hitters/Hitters.csv')
-
-# drop the rows with missing value in Salary
-
-df_hitters=df_hitters.dropna(subset=['Salary'])
- # drop names
-
-df_hitters = df_hitters.drop(columns=df_hitters.columns[0])
-
-
-# Check which clolumns contain missing values
-thresh=0
-droplist = []
-for col in df_hitters.columns:
-    if df_hitters[col].isna().mean() > thresh :
-        droplist.append(col)
-print(droplist)
+from Data_prep import df
 
 # Prepare regression  X features and y response
-X = df_hitters.drop( columns='Salary')
-y = df_hitters['Salary']
+X = df.drop( columns='Salary')
+y = df['Salary']
 
 # Implement Forward selection
 
-model_col = ['Hits']
+model_col = []
 
 remain_col = [ col for col in X.columns if not col in model_col]
 results={}
@@ -39,3 +23,4 @@ for col in remain_col :
     #print('Residual sum of squares: %.2f')
     results[col]=RSS
 print(results)
+
