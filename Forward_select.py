54004c88777a25bd47f2136c00051f8161818bdb
carsten <liese.math@gmail.com>
2019-07-12 22:43:21 +0200

    bugs fixed

--- Forward_select.py
+++ Forward_select.py
@@ -2,34 +2,34 @@ from sklearn.linear_model import LinearRegression
 
 
 def get_models(X, y, model_col):
-    results={}
-    remain_col = [col for col in X.columns if not col in model_col]
-    # Step 1: get the models M_{p+1}
+    results = {}
+    remain_col = [col for col in X.columns if col not in model_col]
+
     for col in remain_col :
 
         X_model = X[[*model_col, col]]
         reg = LinearRegression().fit(X_model, y)
         RSS = ((reg.predict(X_model)-y)**2).sum()
 
-        results[col]=(reg, RSS)
+        results[col] = (reg, RSS)
     return results
 
 
 def choose_best(results):
-    winner = results.keys()[0]
-    val = results[winner][0]
+    winner = sorted(results.keys())[0]
+    val = results[winner][1]
     for key, value in results.items():
         if value[1] <= val:
             winner = key
             val = value[1]
     return winner
-# Step 3: update model_col and store model
 
 
 def update(model_col, models, results):
     winner = choose_best(results)
-    model_col = model_col.append(winner)
-    models[model_col] = results[winner]
+    model_col.append(winner)
+    entry = model_col[:]
+    models[len(model_col)] = (entry, results[winner])
 
     return model_col, models
 
@@ -37,8 +37,9 @@ def update(model_col, models, results):
 def forward_select(X, y):
     model_col = []
     models = {}
-    # while len(model_col) < X.shape[1]:
-    results = get_models(X,y, model_col)
-    # update(model_col, models, results)
+    while len(model_col) < X.shape[1]:
+        results = get_models(X, y, model_col)
+        model_col, models = update(model_col, models, results)
 
-    return results
+    return models
+
