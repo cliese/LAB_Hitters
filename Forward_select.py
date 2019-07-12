73f7f55b6618a500d22cd4aa0e5e2f11e57a63c1
carsten <liese.math@gmail.com>
2019-07-12 15:33:05 +0200

    bugs fixed

--- Forward_select.py
+++ Forward_select.py
@@ -1,9 +1,7 @@
 from sklearn.linear_model import LinearRegression
 
 
-
-
-def get_models(X, model_col ):
+def get_models(X, y, model_col):
     results={}
     remain_col = [col for col in X.columns if not col in model_col]
     # Step 1: get the models M_{p+1}
@@ -21,23 +19,26 @@ def choose_best(results):
     winner = results.keys()[0]
     val = results[winner][0]
     for key, value in results.items():
-        if  value[1] <= val:
+        if value[1] <= val:
             winner = key
             val = value[1]
     return winner
 # Step 3: update model_col and store model
 
+
 def update(model_col, models, results):
     winner = choose_best(results)
-    model_col = models_col.append(winner)
+    model_col = model_col.append(winner)
     models[model_col] = results[winner]
 
     return model_col, models
 
-def forward_select(X):
-    model_col=[]
-    models = {}
-    while len(models_col) < X.shape[1]:
-        results = get_models(X,model_cols)
-        update(model_col, models, results)
 
+def forward_select(X, y):
+    model_col = []
+    models = {}
+    # while len(model_col) < X.shape[1]:
+    results = get_models(X,y, model_col)
+    # update(model_col, models, results)
+
+    return results
